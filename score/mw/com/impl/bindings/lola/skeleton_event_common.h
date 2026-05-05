b42b0eeb7b2ea71cab504a9dc7243ff9c7c116e2
muhseth <muhammad-wahaj.sethi@bmw.de>
2026-05-05 11:03:03 +0200

    adds GetLatestSlot function

--- score/mw/com/impl/bindings/lola/skeleton_event_common.h
+++ score/mw/com/impl/bindings/lola/skeleton_event_common.h
@@ -151,6 +151,7 @@ class SkeletonEventCommon
     std::optional<ProviderEventDataControlLocalView<>> provider_control_local_view_qm_;
     std::optional<ProviderEventDataControlLocalView<>> provider_control_local_view_asil_b_;
     std::optional<ConsumerEventDataControlLocalView<>> consumer_control_local_view_qm_;
+    std::optional<ConsumerEventDataControlLocalView<>> consumer_control_local_view_asil_;
     std::optional<EventDataControlComposite<>> event_data_control_composite_;
 
     EventSlotStatus::EventTimeStamp current_timestamp_;
@@ -214,6 +215,7 @@ void SkeletonEventCommon<SampleType>::PrepareOfferCommon(EventControl& event_con
     {
         auto& provider_control_local_view_asil_b =
             provider_control_local_view_asil_b_.emplace(event_control_asil_b->data_control);
+        score::cpp::ignore = consumer_control_local_view_asil_.emplace(event_control_asil_b->data_control);
         provider_control_local_view_asil_b_ptr = &provider_control_local_view_asil_b;
     }
     score::cpp::ignore =
@@ -298,6 +300,7 @@ void SkeletonEventCommon<SampleType>::PrepareStopOfferCommon() noexcept
     provider_control_local_view_qm_.reset();
     provider_control_local_view_asil_b_.reset();
     consumer_control_local_view_qm_.reset();
+    consumer_control_local_view_asil_.reset();
 }
 
 template <typename SampleType>
